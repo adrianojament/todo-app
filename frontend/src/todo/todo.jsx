330406b5de1af29b63a9015f908952c78f9447dc
Adriano Ament <adriano.ament@sage.com>
2019-07-31 13:04:29 -0300

    Novas alterações

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -4,11 +4,20 @@ import TodoForm from '../todo/todoForm'
 import TodoList from '../todo/todoList'
 
 export default class Todo extends React.Component{
+    constructor(props){
+        super(props)
+        this.handleAdd = this.handleAdd.bind(this)
+    }
+
+    handleAdd(){
+        console.log(this)
+    }
+
     render() {
         return(
             <div>
                 <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
-                <TodoForm/>
+                <TodoForm handleAdd={this.handleAdd}/>
                 <TodoList/>
             </div>
         )
--- frontend/src/todo/todoList.jsx
+++ frontend/src/todo/todoList.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
 export default props => (
-    <div>
-        <h1>Lista</h1>
+    <div role='form' className='todoForm'>
     </div>
 )
